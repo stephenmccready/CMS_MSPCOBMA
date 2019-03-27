9ffe2ad89f9c0f6e8a5281529a5827f723ae7926
Stephen McCready <stephenmccready@gmail.com>
2019-03-27 13:45:51 -0400

    Update Create_tbl_MSPCOBMA_Header.sql

--- Create_tbl_MSPCOBMA_Header.sql
+++ Create_tbl_MSPCOBMA_Header.sql
@@ -1,9 +1,10 @@
+If Object_ID('[dbo].[tbl_MSPCOBMA_Header]') Is Not Null Drop Table [dbo].[tbl_MSPCOBMA_Header]
 CREATE TABLE [dbo].[tbl_MSPCOBMA_Header](
 	[MSPCOBMAHeaderId] [int] IDENTITY(1,1) NOT NULL,
 	[HeaderCode] [varchar](8) NULL,
-	[SendingEntity] [varchar](8) NULL,
+	[SendingEntity] [varchar](4) NULL,
 	[FileCreationDate] [varchar](8) NULL,
-	[Filler1] [varchar](700) NULL,
+	[Filler1] [varchar](680) NULL,
 	[DateImported] [datetime] NOT NULL,
 	[FileName] [varchar](128) NOT NULL
 ) ON [PRIMARY]
